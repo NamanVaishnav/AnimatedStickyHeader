84567b3b791165047760fe1b57f63878e1565faa
Naman Vaishnav <namanmvaishnav@gmail.com>
2024-01-05 15:23:51 +0530

    Initial Commit

---
+++ AnimatedStickyHeader/AnimatedStickyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedStickyHeaderApp.swift
+//  AnimatedStickyHeader
+//
+//  Created by Naman Vaishnav on 05/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedStickyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
